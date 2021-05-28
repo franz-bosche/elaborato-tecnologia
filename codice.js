3f7d3e8ef41677acf4ac2f55ece4c6a655f9d4a4
Francesco Boscherini <franz.bosche@gmail.com>
2021-05-28 20:17:01 +0200

    Implementazione funzione palindromo

--- codice.js
+++ codice.js
@@ -1,1 +1,16 @@
-console.log('prova');
+function palindromo(parola) {
+    let termineMedio = parola.length / 2;
+    for (let indice1 = 0; indice1 < termineMedio; indice1++) {
+        let indice2 = parola.length - 1 - indice1;
+        let carattere1 = parola[indice1];
+        let carattere2 = parola[indice2];
+        if (carattere1 !== carattere2) {
+        return false;
+        }
+    }
+    return true;
+}
+
+console.log(palindromo('anna'));
+console.log(palindromo('aveva'));
+console.log(palindromo('prova'));
