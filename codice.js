68430704c4b6c28d0029cb8ca270e368e67aead1
Francesco Boscherini <franz.bosche@gmail.com>
2021-05-28 20:54:28 +0200

    Prima implementazione funzionale

--- codice.js
+++ codice.js
@@ -1,9 +1,9 @@
-function palindromo(parola) {
-    let termineMedio = parola.length / 2;
+function palindromo(testo) {
+    let termineMedio = testo.length / 2;
     for (let indice1 = 0; indice1 < termineMedio; indice1++) {
-        let indice2 = parola.length - 1 - indice1;
-        let carattere1 = parola[indice1];
-        let carattere2 = parola[indice2];
+        let indice2 = testo.length - 1 - indice1;
+        let carattere1 = testo[indice1];
+        let carattere2 = testo[indice2];
         if (carattere1 !== carattere2) {
         return false;
         }
@@ -11,6 +11,18 @@ function palindromo(parola) {
     return true;
 }
 
-console.log(palindromo('anna'));
-console.log(palindromo('aveva'));
-console.log(palindromo('prova'));
+function inizializzaPagina() {
+    let pulsante = document.getElementById('controlla');
+    let testo = document.getElementById('testo');
+    let risultato = document.getElementById('risultato');
+    
+    pulsante.addEventListener('click', function() {
+        if (palindromo(testo.value)) {
+            risultato.innerText = testo.value + ' è palindromo';
+        } else {
+            risultato.innerText = testo.value + ' non è palindromo';
+        }
+    });
+}
+
+inizializzaPagina();
